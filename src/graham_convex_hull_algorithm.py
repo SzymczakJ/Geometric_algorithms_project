acb647b3872558e0df18c9aa364775fd3787dd7e
szymonbudziak@gmail.com <szymonbudziak@gmail.com>
2022-01-03 22:39:30 +0100

    created most of the algorithms with visualizations

---
+++ src/graham_convex_hull_algorithm.py
@@ -0,0 +1,75 @@
+from functools import cmp_to_key
+from visualization.visualization_tool import *
+from additional_functions.additional_functions import *
+from copy import deepcopy
+
+
+def graham_convex_hull(points, epsilon=10 ** (-12), write_to_file=False, filename="graham_result"):
+    lowest_point = points[0]
+    for i in range(len(points)):
+        if abs(lowest_point[1] - points[i][1]) < epsilon:
+            if lowest_point[0] > points[i][0]:
+                lowest_point = points[i]
+        elif lowest_point[1] > points[i][1]:
+            lowest_point = points[i]
+
+    def graham_cmp(point_a, point_b):
+        if point_a[0] == lowest_point[0] and point_a[1] == lowest_point[1]:
+            return -1
+        elif point_b[0] == lowest_point[0] and point_b[1] == lowest_point[1]:
+            return 1
+        orient = orientation(lowest_point, point_a, point_b, epsilon)
+        if orient < -epsilon:
+            return 1
+        elif epsilon > orient > -epsilon:
+            if point_a[1] > point_b[1]:
+                return 1
+            elif point_a[1] < point_b[1]:
+                return -1
+            elif abs(point_a[0] - lowest_point[0]) > abs(point_b[0] - lowest_point[0]):
+                return 1
+            elif abs(point_a[0] - lowest_point[0]) < abs(point_b[0] - lowest_point[0]):
+                return -1
+            else:
+                return 0
+        else:
+            return -1
+
+    graham_cmp_key = cmp_to_key(graham_cmp)
+    sorted_set = sorted(points, key=graham_cmp_key)
+    convex_hull = [sorted_set[0], sorted_set[1]]
+    convex_hull_lines = [[sorted_set[0], sorted_set[1]]]
+    i = 2
+
+    scenes = [Scene(points=[PointsCollection(deepcopy(points), color="black")])]
+    scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black")],
+                        lines=[LinesCollection(deepcopy(convex_hull_lines), color="blue"),
+                               LinesCollection([(convex_hull[-1], convex_hull[-2])], color="red")]))
+
+    while i < len(points):
+        if (convex_hull[0][0] == convex_hull[-1][0] and convex_hull[0][1] == convex_hull[-1][1]) or \
+                orientation(convex_hull[-2], convex_hull[-1], sorted_set[i], epsilon) > epsilon:
+            convex_hull.append(sorted_set[i])
+            convex_hull_lines.append([convex_hull[-2], convex_hull[-1]])
+            i += 1
+        else:
+            convex_hull.pop()
+            convex_hull_lines.pop()
+        if len(convex_hull) > 1:
+            scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black"),
+                                        PointsCollection(deepcopy(convex_hull), color="blue")],
+                                lines=[LinesCollection(deepcopy(convex_hull_lines), color="blue"),
+                                       LinesCollection([(convex_hull[-1], convex_hull[-2])], color="red")]))
+        else:
+            scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black"),
+                                        PointsCollection(deepcopy(convex_hull), color="blue")],
+                                lines=[LinesCollection(deepcopy(convex_hull_lines), color="blue")]))
+    convex_hull_lines.append([convex_hull[0], convex_hull[-1]])
+    scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black"),
+                                PointsCollection(deepcopy(convex_hull), color="blue")],
+                        lines=[LinesCollection(deepcopy(convex_hull_lines), color="blue")]))
+    if write_to_file:
+        with open(f'{filename}.txt', 'w') as file:
+            for item in convex_hull:
+                file.write(f"{item}\n")
+    return convex_hull, scenes
---
+++ src/upper_lower_convex_hull_algorithm.py
@@ -0,0 +1,72 @@
+from visualization.visualization_tool import *
+from additional_functions.additional_functions import *
+from copy import deepcopy
+
+
+def upper_lower_convex_hull(points, epsilon=10 ** (-12), write_to_file=False, filename="upper_lower_result"):
+    if len(points) < 3:
+        return None, None
+    sorted_points = sorted(points, key=lambda x: (x[0], x[1]))
+
+    scenes = [Scene(points=[PointsCollection(deepcopy(points), color="black")])]
+
+    # upper convex hull
+    upper_hull = [sorted_points[0], sorted_points[1]]
+    upper_hull_lines = [(sorted_points[0], sorted_points[1])]
+
+    for idx, value in enumerate(sorted_points[2:]):
+        scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black"),
+                                    PointsCollection([upper_hull[-1]], color="blue"),
+                                    PointsCollection([upper_hull[-2]], color="blue"),
+                                    PointsCollection(deepcopy(upper_hull), color="green")],
+                            lines=[LinesCollection([[upper_hull[-2], value]], color="red"),
+                                   LinesCollection([[upper_hull[-1], value]], color="red"),
+                                   LinesCollection(deepcopy(upper_hull_lines), color="green")]))
+        while len(upper_hull) > 1 and orientation(upper_hull[-2], upper_hull[-1], value, epsilon) != 1:
+            upper_hull.pop()
+            upper_hull_lines.pop()
+            scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black"),
+                                        PointsCollection(deepcopy(upper_hull), color="green"),
+                                        PointsCollection([value], color="red")],
+                                lines=[LinesCollection(deepcopy(upper_hull_lines), color="red"),
+                                       LinesCollection([[upper_hull[-1], value]], color="yellow")]))
+        upper_hull.append(value)
+        upper_hull_lines.append((upper_hull[-2], upper_hull[-1]))
+
+    # lower convex hull
+    lower_hull = [sorted_points[0], sorted_points[1]]
+    lower_hull_lines = [(sorted_points[0], sorted_points[1])]
+
+    for idx, value in enumerate(sorted_points[2:]):
+        scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black"),
+                                    PointsCollection([lower_hull[-1]], color="blue"),
+                                    PointsCollection([lower_hull[-2]], color="blue"),
+                                    PointsCollection(deepcopy(lower_hull), color="green")],
+                            lines=[LinesCollection(deepcopy(upper_hull_lines), color="blue"),
+                                   LinesCollection(deepcopy(lower_hull_lines), color="red"),
+                                   LinesCollection([[lower_hull[-2], value]], color="red"),
+                                   LinesCollection([[lower_hull[-1], value]], color="red")]))
+        while len(lower_hull) > 1 and orientation(lower_hull[-2], lower_hull[-1], value, epsilon) != -1:
+            lower_hull.pop()
+            lower_hull_lines.pop()
+            scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black"),
+                                        PointsCollection(deepcopy(lower_hull), color="green"),
+                                        PointsCollection([value], color="red")],
+                                lines=[LinesCollection(deepcopy(upper_hull_lines), color="blue"),
+                                       LinesCollection(deepcopy(lower_hull_lines), color="red"),
+                                       LinesCollection([[lower_hull[-1], value]], color="yellow")]))
+        lower_hull.append(value)
+        lower_hull_lines.append((lower_hull[-2], lower_hull[-1]))
+
+    scenes.append(Scene(points=[PointsCollection(deepcopy(points), color="black")],
+                        lines=[LinesCollection(deepcopy(upper_hull_lines), color="blue"),
+                               LinesCollection(deepcopy(lower_hull_lines), color="green")]))
+    lower_hull.reverse()
+    upper_hull.pop()
+    lower_hull.pop()
+    upper_hull.extend(lower_hull)
+    if write_to_file:
+        with open(f'{filename}.txt', 'w') as file:
+            for item in upper_hull:
+                file.write(f"{item}\n")
+    return upper_hull, scenes
